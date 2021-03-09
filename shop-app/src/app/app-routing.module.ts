cb3d0351374102c6b798bf85422b1376016d037b
daniel-bar <72673770+daniel-bar@users.noreply.github.com>
2021-03-09 10:27:53 +0200

    new

--- shop-app/src/app/app-routing.module.ts
+++ shop-app/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AboutComponent } from './components/pages/about/about.component';
 import { ContactComponent } from './components/pages/contact/contact.component';
 import { ProductComponent } from './components/pages/product/product.component';
 import { RegisterComponent } from './components/pages/register/register.component';
+import { LoginComponent } from './components/pages/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'contact', component: ContactComponent },
   { path: 'product', component: ProductComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
--- shop-app/src/app/app.module.ts
+++ shop-app/src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/shared/nav/nav.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
-// import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { ContactComponent } from './components/pages/contact/contact.component';
 import { ProductComponent } from './components/pages/product/product.component';
 import { RegisterComponent } from './components/pages/register/register.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { ProfileComponent } from './components/pages/profile/profile.component';
 
 
 @NgModule({
@@ -18,11 +19,12 @@ import { RegisterComponent } from './components/pages/register/register.componen
     AppComponent,
     NavComponent,
     FooterComponent,
-    // HomeComponent,
     AboutComponent,
     ContactComponent,
     ProductComponent,
     RegisterComponent,
+    LoginComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
